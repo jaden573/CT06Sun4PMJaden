415028042cba6e6bbb4f993b23dff05170bc1d1a
jaden573 <ganshiongteckjaden@gmail.com>
2026-07-12 09:08:28 +0000

    Jul 12, 2026, 9:08 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -34,7 +34,7 @@ function setup() {
     // ellipse(295,115,53,53);
     // ellipse(126,560,32,32);
     // line(100,200,300,400)
-    triangle(100)
+    triangle(400,600,800,)
 
   // Challenge 4: A rocketship using triangle() function
 

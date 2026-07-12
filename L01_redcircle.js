8a5413ba513210ef78923bca280f272798f3b462
jaden573 <ganshiongteckjaden@gmail.com>
2026-07-12 08:03:33 +0000

    Jul 12, 2026, 8:03 AM

--- L01_redcircle.js
+++ L01_redcircle.js
@@ -4,8 +4,24 @@ function setup(){
 }
 
 function draw(){
-    fill(255, 0, 0);
-    ellipse(300,200,200,200);
+    fill(155, 0, 0);
+    ellipse(300,100,120,120); //ellipse(x,y,THICCNESS, height)
     
     // Todo: Challenge 3 Traffic Light
+    fill(255,255,0);
+    ellipse(300,220,120,120);
+
+    fill(10,300,0)
+    ellipse(300,340,120,120)
+
+    fill(0,0,0)
+    ellipse(180,220,120,120)
+
+    fill(0,0,0)
+    ellipse(420,220,120,120)
+
+
+
+
+
 }

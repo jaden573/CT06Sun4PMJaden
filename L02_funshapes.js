e957661eb8df0a6f4863a1cc568d7695d886eedd
jaden573 <ganshiongteckjaden@gmail.com>
2026-07-12 09:26:48 +0000

    Jul 12, 2026, 9:26 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -45,6 +45,7 @@ quad(450,100,750,100,750,400,450,400)
 ellipse(525,200,40,40)
   ellipse(675,200,40,40)
   quad(525,300,675,300,650,325,550,325)
+  ellipse()
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon

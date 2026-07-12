3e31a612f9a79cd603cdc633d6dba3bfdd46c516
jaden573 <ganshiongteckjaden@gmail.com>
2026-07-12 08:26:32 +0000

    Jul 12, 2026, 8:26 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -7,9 +7,8 @@ function setup() {
 }
 function draw() {
    ellipse(600,250,400,200);
-  ellipse(600,250,100,50);
   ellipse(600,250,100,200);
- 
+ ellipse(600,250,100,50);
   // Challenge 1: An eye using ellipse() function
 //to create ellipse, ellipse(x,y,width,height)
   // Challenge 2: A face using circle() function

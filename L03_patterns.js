12213a29ebc449301d57fde87517a06125197b7c
jaden573 <ganshiongteckjaden@gmail.com>
2026-07-19 08:10:21 +0000

    Jul 19, 2026, 8:10 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -7,7 +7,9 @@ function setup() {
 function draw() {
   // Recap 1: Repeating Circles
   ellipse(75,200,100);
-  ellipse(225,200,100)
+  ellipse(225,200,100);
+  ellipse(375,200,100);
+  elli[]
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop

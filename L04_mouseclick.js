d5427790a5720f80ff0b61b99d461a8075960572
jaden573 <ganshiongteckjaden@gmail.com>
2026-07-26 08:55:01 +0000

    Jul 26, 2026, 8:55 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -25,5 +25,5 @@ function mousePressed(){
 function mouseReleased(){
     shapecolour = color(random(255),random(255),random(255));
     rect(random(600),random(400),random(60),random(40))
-    background(220)
+    background(220);
 }

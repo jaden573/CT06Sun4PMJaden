4c950c3db8643badde0b5bdda0837c90260dd552
jaden573 <ganshiongteckjaden@gmail.com>
2026-07-26 08:58:20 +0000

    Jul 26, 2026, 8:58 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -3,7 +3,7 @@ let shapecolour = 'blue'
 
 function setup(){
     createCanvas(600,400);
-    background(220);  ell
+    background(220);  
 }
 
 function draw(){
@@ -13,11 +13,7 @@ function draw(){
     //random(one number); ->generate num between 0 and the number
     //random(low num, high num); -> gen num between low and high num
     //fill(r,g,b); between 0 and 255
-    if(mousePressed){
-        function mouseMoved(){
-          ellipse(mouseX,mouseY,10,10)
-        }
-    }
+    
 }
 
 // function mousePressed(){

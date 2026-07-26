135eb1d4a2077d327dbfb8b3a58926d3ac1b016e
jaden573 <ganshiongteckjaden@gmail.com>
2026-07-26 08:40:30 +0000

    Jul 26, 2026, 8:40 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -11,7 +11,7 @@ function draw(){
     circle(300,200,100);
     //random(); -> generate a num between 0 and 1
     //random(one number); ->generate num between 0 and the number
-    //random(low num, high num); ->
+    //random(low num, high num); -> 
 }
 
 function mousePressed(){

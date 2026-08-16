8eb5019775a8fa0d25fa6c0a2435d44bf5f6382b
jaden573 <ganshiongteckjaden@gmail.com>
2026-08-16 08:19:38 +0000

    Aug 16, 2026, 8:19 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -22,5 +22,6 @@ function keyPressed () {
         let y = random(height)
         let size = random(5,40)
         fill(random(255),random(255),random(255))
-        circle(x,y,size)
+        circle(x,y,size);
+        
 }

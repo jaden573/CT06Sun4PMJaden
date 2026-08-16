85f8eab3c10496b8502ca476a9cbfdca72a8302f
jaden573 <ganshiongteckjaden@gmail.com>
2026-08-16 09:06:58 +0000

    Aug 16, 2026, 9:06 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -8,15 +8,15 @@ function setup () {
     ballcolor=color(255,0,0)
 }
 
-function mousePressed() {
-    colors = color(random(255), random(255), random(255));
-    size = 5;
-}
-function mouseDragged() {
-    fill(colors)
-    circle(mouseX,mouseY,size)
-    size+=0.5
-}
+// function mousePressed() {
+//     colors = color(random(255), random(255), random(255));
+//     size = 5;
+// }
+// function mouseDragged() {
+//     fill(colors)
+//     circle(mouseX,mouseY,size)
+//     size+=0.5
+// }
 
 //================================================================
 function keyPressed () {

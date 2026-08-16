3e90cf42138bf26cad997cfd05c71fd9315ebb30
jaden573 <ganshiongteckjaden@gmail.com>
2026-08-16 09:08:12 +0000

    Aug 16, 2026, 9:08 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -19,17 +19,17 @@ function setup () {
 // }
 
 //================================================================
-function keyPressed () {
-    background(220);
-    let numCircles = floor(random(5,21))
-    for(let i=0; i < numCircles;i++){
-        let x = random(width)
-        let y = random(height)
-        let size = random(5,40)
-        fill(random(255),random(255),random(255))
-        circle(x,y,size);
-    }
-}
+// function keyPressed () {
+//     background(220);
+//     let numCircles = floor(random(5,21))
+//     for(let i=0; i < numCircles;i++){
+//         let x = random(width)
+//         let y = random(height)
+//         let size = random(5,40)
+//         fill(random(255),random(255),random(255))
+//         circle(x,y,size);
+//     }
+// }
 function keyReleased(){
     background(220);
     console.log("key:",key);

3c9e4fbafa1542641b1ff923ea6093075da8168e
jaden573 <ganshiongteckjaden@gmail.com>
2026-08-16 08:38:14 +0000

    Aug 16, 2026, 8:38 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -29,7 +29,7 @@ function keyPressed () {
 function keyReleased(){
     beckground(220);
     console.log("key:",key);
-    console.log
+    console.log("keycode:")
 }
 function draw(){
     background(220);

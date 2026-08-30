52ab25b33d616832e5026c6b2efa6cbc763dc64d
jaden573 <ganshiongteckjaden@gmail.com>
2026-08-30 08:28:09 +0000

    Aug 30, 2026, 8:28 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,40 +1,41 @@
-let pico, popSound;
-let x = 200;
-let y = 200;
-let picowidth = 100;
-let ballx, bally, ballspeed;
-function preload(){
-    pico = loadImage('assets/pico-a.png');
-    popSound = loadSound('assets/pop.mp3');
-}
-function setup (){
-    createCanvas(400,400);
-    background(220);
-}
-function draw(){
-    background(220);
-    if(keyIsDown(LEFT_ARROW)){
-        x-=3;
-    }
-    if(keyIsDown(RIGHT_ARROW)){
-        x+=3;
-    }
-    if(keyIsDown(UP_ARROW)){
-        y-=3;
-    }
-    if(keyIsDown(DOWN_ARROW)){
-        y+=3;
-    }
-    image(pico,x,y,picowidth,100);
-    }
-  function keyPressed(){
-        if(key=== " "){
-            picowidth /=2;
-            popSound.loop ();
-        }
-    }
-    function draw(){
-        background(220)
+// let pico, popSound;
+// let x = 200;
+// let y = 200;
+// let picowidth = 100;
+// let ballx, bally, ballspeed;
+// function preload(){
+//     pico = loadImage('assets/pico-a.png');
+//     popSound = loadSound('assets/pop.mp3');
+// }
+// function setup (){
+//     createCanvas(400,400);
+//     background(220);
+// }
+// function draw(){
+//     background(220);
+//     if(keyIsDown(LEFT_ARROW)){
+//         x-=3;
+//     }
+//     if(keyIsDown(RIGHT_ARROW)){
+//         x+=3;
+//     }
+//     if(keyIsDown(UP_ARROW)){
+//         y-=3;
+//     }
+//     if(keyIsDown(DOWN_ARROW)){
+//         y+=3;
+//     }
+//     image(pico,x,y,picowidth,100);
+//     }
+//   function keyPressed(){
+//         if(key=== " "){
+//             picowidth /=2;
+//             popSound.loop ();
+//         }
+//     }
+//     function draw(){
+//         background(220)
         
         
-    }
+//     }
+

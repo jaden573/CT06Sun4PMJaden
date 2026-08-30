d0003c251f4bd648f7e16272b5be81dce34b4e1e
jaden573 <ganshiongteckjaden@gmail.com>
2026-08-30 08:22:07 +0000

    Aug 30, 2026, 8:22 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,10 +1,33 @@
-let pico, popSound
-let x = 200
-
+let pico, popSound;
+let x = 200;
+let y = 200;
+let picowidth = 100;
 function preload(){
-
+    pico = loadImage('assets/pico-a.png');
+    popSound = loadSound('assets/bossaNova.mp3');
 }
 function setup (){
-    createCanvas(600,400)
-    background(220)
+    createCanvas(400,400);
+    background(220);
 }
+function draw(){
+    background(220);
+    if(keyIsDown(LEFT_ARROW)){
+        x-=3;
+    }
+    if(keyIsDown(RIGHT_ARROW)){
+        x+=3;
+    }
+    if(keyIsDown(UP_ARROW)){
+        y-=3;
+    }
+    if(keyIsDown(DOWN_ARROW)){
+        y+=3;
+    }
+    function keyPressed(){
+        if(key=== ""){
+            picowidth /=2;
+            popSound.play ();
+        }
+    }
+    }

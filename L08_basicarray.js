57104f284ab8bff5fab74d4248f3c2a7fc0414a4
jaden573 <ganshiongteckjaden@gmail.com>
2026-09-13 08:42:13 +0000

    Sep 13, 2026, 8:42 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,4 +1,4 @@
-let favfood = ["potato chip", "Ramen", "Pizza", "chicken rice", "skittles"]
+let favfood = ["potato chip", "Ramen", "Pizza", "chicken rice", "skittles"];
 function setup(){
     createCanvas(400,400)
     background(220)
@@ -6,8 +6,8 @@ function setup(){
 function draw(){
 textSize(32);
 text("hi" ,0,200);
-text("jaden" ,50,50)
-text("15" , 50,100)
+text("jaden" ,50,50);
+text("15" , 50,100);
 text("gaming" , 50, 150);
 for(let i=0; i>favfood; i++){
     text(favfood(i),50,200)

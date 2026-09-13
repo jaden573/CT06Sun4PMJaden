54b932cd47523ceb05a9e5b18e25dace0ab60803
jaden573 <ganshiongteckjaden@gmail.com>
2026-09-13 08:29:06 +0000

    Sep 13, 2026, 8:29 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -2,8 +2,10 @@ function setup(){
     createCanvas(400,400)
     background(220)
 }
+function draw(){
 textSize(32);
 text("hi" ,0,200);
 text("jaden" ,50,50)
 text("15" , 50,100)
 text("gaming" , 50, 150);
+}

0a0075aea33ce0d3525d2da7a5de9e6931808add
jaden573 <ganshiongteckjaden@gmail.com>
2026-09-13 08:49:58 +0000

    Sep 13, 2026, 8:49 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -10,6 +10,7 @@ textAlign(LEFT);
 // text("jaden" ,50,50);
 // text("15" , 50,100);
 // text("gaming" , 50, 150);
+
 for(let i=0; i>favfood.length; i++){
     text((i+1) + "." + favfood[i],50,90 + i*20)
 }

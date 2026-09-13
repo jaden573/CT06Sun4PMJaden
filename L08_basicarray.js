3e4a9258c758aea3adf88b3050cb23379c2180a4
jaden573 <ganshiongteckjaden@gmail.com>
2026-09-13 08:37:34 +0000

    Sep 13, 2026, 8:37 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,4 +1,4 @@
-let favfood = ["potato chip", "Ramen", "Pizza", "chicken rice"]
+let favfood = ["potato chip", "Ramen", "Pizza", "chicken rice", "skittles"]
 function setup(){
     createCanvas(400,400)
     background(220)

49a8b37c10ee8f5352685e67c3751f00d9bd4e8e
jaden573 <ganshiongteckjaden@gmail.com>
2026-09-13 08:57:41 +0000

    Sep 13, 2026, 8:57 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -14,7 +14,7 @@ textAlign(LEFT);
 // text("gaming" , 50, 150);
 text("my favourite food:", 50,50)
 for(let i=0; i<favfood.length; i++){
-    text((i+1) + "." + favfood[i],50,90 + i*20)
+    text((i+1) + "." + favfood[i],yPOS + i*20)
 }
 yPOS+=10
 }

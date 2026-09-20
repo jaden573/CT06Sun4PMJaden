8be04ebe486b2d26065de89d88194c93e3b45d18
jaden573 <ganshiongteckjaden@gmail.com>
2026-09-20 08:38:31 +0000

    Sep 20, 2026, 8:38 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,26 +1,29 @@
 // write your codes here
-let x = 0
-let y = 0
-let speed = 2
-function setup(){
-    createCanvas(600,400)
-    background(220)
-}
-function draw(){
-    background(0)
-    textSize(20)
-    fill(255,0,0)
+// let x = 0
+// let y = 0
+// let speed = 2
+// function setup(){
+//     createCanvas(600,400)
+//     background(220)
+// }
+// function draw(){
+//     background(0)
+//     textSize(20)
+//     fill(255,0,0)
    
-    text("Bounce!",x ,200)
-    x+=speed
-    if(x< width - 60||x-60>0){
-        speed = -speed
-    }
-    let h = hour();
-    let m = minute();
-    let s = second();
-    let string = nf(h,2) + nf(m,2) + nf(s,2);
-    textSize(32)
-    fill("gold")
-    text(string,100,200);
+//     text("Bounce!",x ,200)
+//     x+=speed
+//     if(x< width - 60||x-60>0){
+//         speed = -speed
+//     }
+//     let h = hour();
+//     let m = minute();
+//     let s = second();
+//     let string = nf(h,2) + nf(m,2) + nf(s,2);
+//     textSize(32)
+//     fill("gold")
+//     text(string,100,200);
+// }
+function draw(){
+    
 }

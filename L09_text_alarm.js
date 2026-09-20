464d2283d0a74d1d3fab8e8301d4cfd4cebb4ac6
jaden573 <ganshiongteckjaden@gmail.com>
2026-09-20 08:52:20 +0000

    Sep 20, 2026, 8:52 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -48,7 +48,7 @@ function mousePressed(){
             interval = setInterval(updateCountdown, 500)
             function updateCountdown(){
                 background(random(220))
-                sound
+                
             }
         }
     }
